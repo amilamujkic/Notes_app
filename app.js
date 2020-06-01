22e82b93b93d4a38adff446103cd657295d36c80
amilamjk <amilamjk@gmail.com>
2020-06-01 17:04:42 +0200

    day 1

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const message = getNotes();
 
 console.log(message);
 console.log(validator.isURL('https/nead.io'));
-
+console.log(chalk.green.inverse.bold('Success!'));
 
 
 
