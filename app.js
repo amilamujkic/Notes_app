f23d3123dae95e3b00bc7680e82c94132d4ae5b4
amilamjk <amilamjk@gmail.com>
2020-06-02 17:14:21 +0200

    Day 2

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const message = getNotes();
+yargs.version('1.1.0');
 
-console.log(message);
-console.log(validator.isURL('https/nead.io'));
-console.log(chalk.green.inverse.bold('Success!'));
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body );
+    }
+});
 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note!');
+    }
+});
 
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function() {
+        console.log('Listing out all notes!');
+    }
+});
 
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note!');
+    }
+});
 
+yargs.parse();
